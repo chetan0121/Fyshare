d9f0fbc4ae71d00f1eb1c3aa1714550e59d51e9c
chetan0121 <prajapatichetandilipbhai@gmail.com>
2026-04-08 00:03:58 +0530

    Type changes and improve efficiency - Extend normal classes like Color, Bg to IntEnum for const-ness - Cache valid fgs and valid bgs at class lvl for performance - refector strip() for more robustness - Other minor imrpovements

--- core/utils/style_manager.py
+++ core/utils/style_manager.py
@@ -1,125 +1,10 @@
 import re
 import sys
-from typing import Union, Optional
+from typing import Final, Union, Optional
+from enum import IntEnum
+from . import helper
 
-def _parse_int(txt) -> Optional[int]:
-    try:
-        num = int(txt)
-        return num
-    except (TypeError, ValueError):
-        return None
-
-class Style:
-    ESC = "\033["
-    RESET = f"{ESC}0m"
-
-    @staticmethod
-    def _to_escape(codes: list[str]):
-        """Convert Numbers to ANSI escape code if valid"""
-        if not codes:
-            return ""
-        return f"{Style.ESC}{';'.join(str(c) for c in codes)}m"
-    
-    @staticmethod
-    def _resolve(codes: Union[list, tuple, set]) -> list:
-        """
-        Handle Code list and resolve custom codes
-        """
-        # Check if empty
-        if not codes:
-            return None
-        
-        # Extract all valid integers
-        raw_codes = []
-        for c in codes:
-            num = _parse_int(c)
-            if num is not None and num >= 0:
-                raw_codes.append(num)
-
-        # Cache valid color values
-        valid_fgs = [int(v) for k, v in Color.__dict__.items() if not k.startswith('_')]
-        valid_bgs = [int(v) for k, v in Bg.__dict__.items() if not k.startswith('_')]
-
-        # State trackers
-        color = None
-        bg = None
-        apply_bright_color = False
-        apply_bright_bg = False
-        normal_codes = []
-
-        # Classify each code
-        for code in raw_codes:
-            if code in valid_fgs:
-                color = code
-            elif code in valid_bgs:
-                bg = code
-            elif code == TextStyle.BRIGHT:
-                apply_bright_color = True
-            elif code == TextStyle.BRIGHT_BG:
-                apply_bright_bg = True
-            else:
-                normal_codes.append(code)
-
-        # Apply brightness if requested
-        if color is not None:
-            if apply_bright_color and color != Color.DEFAULT:
-                color += 60
-            normal_codes.append(color)    
-
-        if bg is not None:
-            if apply_bright_bg and bg != Bg.DEFAULT:
-                bg += 60    
-            normal_codes.append(bg)    
-
-        return normal_codes
-    
-    @staticmethod
-    def strip(text: str) -> str:
-        """Remove all ANSI escape codes from text."""
-        return re.sub(r"\033\[[0-9;]*m", "", text)
-    
-    @staticmethod
-    def styled(text: str, *styles) -> str:
-        """
-        Return Styled Text wrapped with ANSI escape codes
-        
-        :param text: Text to be styled
-        :param styles: Styles to be applied, tuple of raw integers (from ANSI codes)
-            
-        Usage: 
-            1. red_txt = styled("I am Bold Red", Colors.RED, TextStyle.BOLD)
-            2. yellow_txt = styled("Manually styling, Bold yellow txt", 1, 33)
-        """
-        # Check if empty
-        if not styles:
-            return text
-        
-        resolved = Style._resolve(styles)
-        escape_seq = Style._to_escape(resolved)
-
-        return f"{escape_seq}{text}{Style.RESET}"
-    
-    @staticmethod
-    def print_style(txt="", *codes, prefix="", end="\n"):
-        """
-        Prints styled text to the console using ANSI escape codes.
-
-        Args:
-            - txt: The text to print. Defaults to an empty string.
-            - *ansi_codes: Variable-length list of ANSI codes (int/str)
-                        to style the text. Example: 31 for red, 1 for bold.
-            - end: String appended after the text. Defaults to a newline.
-        Notes:
-            - ANSI codes are applied to the text only; 
-                `end` and 'prefix' are printed as-is.
-            - If no valid ANSI codes are provided, the text is printed as it is.
-            - Invalid ANSI codes (non-integer/non-string/non-ANSI) are ignored.
-        """
-        msg = Style.styled(txt, *codes)
-        sys.stdout.write(f"{prefix}{msg}{end}")
-
-
-class TextStyle:
+class TextStyle(IntEnum):
     """Text styles"""
     BOLD      = 1
     DIM       = 2
@@ -131,8 +16,13 @@ class TextStyle:
     # Custom style
     BRIGHT = 200
     BRIGHT_BG = 300
+    
+    # Allows using enum values as callable methods
+    # e.g. TextStyle.BOLD("Hello")
+    def __call__(self, text):
+        return Style.styled(text, self)
 
-class Color:
+class Color(IntEnum):
     """All Foreground color codes(integers)"""
     BLACK   = 30
     RED     = 31
@@ -147,8 +37,13 @@ class Color:
     PURPLE  = MAGENTA
     RESET = 39
     DEFAULT = 39
+    
+    # Allows using enum values as callable methods
+    # e.g. Color.BLUE("Hello")
+    def __call__(self, text):
+        return Style.styled(text, self)
 
-class Bg:
+class Bg(IntEnum):
     """All Background color codes(integers)"""
     BLACK   = 40
     RED     = 41
@@ -163,3 +58,126 @@ class Bg:
     PURPLE  = MAGENTA
     RESET = 49
     DEFAULT = 49
+    
+    # Allows using enum values as callable methods
+    # e.g. Bg.BLUE("Hello")
+    def __call__(self, text):
+        return Style.styled(text, self)
+    
+
+class Style:
+    ESC: Final = "\033["
+    RESET: Final = f"{ESC}0m"
+    
+    _VALID_ANSI: Final = re.compile(r'\033\[[0-?]*[ -/]*[@-~]')
+    
+    # Cache valid color values
+    _VALID_FGS = (int(v) for k, v in Color.__dict__.items() if not k.startswith('_'))
+    _VALID_BGS = (int(v) for k, v in Bg.__dict__.items() if not k.startswith('_'))
+
+    @staticmethod
+    def _to_escape(codes: Optional[list[str]]):
+        """Convert Numbers to ANSI escape code if valid"""
+        if not codes:
+            return ""
+        return f"{Style.ESC}{';'.join(str(c) for c in codes)}m"
+    
+    @staticmethod
+    def _resolve(codes: Union[list, tuple, set]) -> Optional[list]:
+        """
+        Handle Code list and resolve custom codes
+        """
+        # Check if empty
+        if not codes:
+            return None
+        
+        # Extract all valid integers
+        raw_codes = []
+        for c in codes:
+            num = helper.try_parse_int(c)
+            if num is not None and num >= 0:
+                raw_codes.append(num)
+
+        # State trackers
+        color = None
+        bg = None
+        bright_fg = False
+        bright_bg = False
+        normal_codes = []
+
+        # Classify each code
+        for code in raw_codes:
+            if code in Style._VALID_FGS:
+                color = code
+            elif code in Style._VALID_BGS:
+                bg = code
+            elif code == TextStyle.BRIGHT:
+                bright_fg = True
+            elif code == TextStyle.BRIGHT_BG:
+                bright_bg = True
+            else:
+                normal_codes.append(code)
+
+        # Apply brightness if requested
+        if color is not None:
+            if bright_fg and color != Color.DEFAULT:
+                color += 60
+            normal_codes.append(color)    
+
+        if bg is not None:
+            if bright_bg and bg != Bg.DEFAULT:
+                bg += 60    
+            normal_codes.append(bg)    
+
+        return normal_codes
+    
+    @staticmethod
+    def strip(text: str) -> str:
+        """Remove all ANSI escape codes from text."""
+        if not text:
+            return ""
+        return Style._VALID_ANSI.sub('', text)
+    
+    @staticmethod
+    def styled(text: str, *styles) -> str:
+        """
+        Return Styled Text wrapped with ANSI escape codes
+        
+        :param text: Text to be styled
+        :param styles: Styles to be applied, like tuple of integers, 
+        
+        Usage: 
+            1. red_txt = styled("I am Bold Red", Colors.RED, TextStyle.BOLD)
+            2. yellow_txt = styled("Manually styling, Bold yellow txt", 1, 33)
+        """
+        if not isinstance(text, str):
+            raise TypeError("text must be type str")
+        
+        # Check if empty
+        if not styles:
+            return text
+        
+        resolved = Style._resolve(styles)
+        escape_seq = Style._to_escape(resolved)
+
+        return f"{escape_seq}{text}{Style.RESET}"
+    
+    @staticmethod
+    def print(txt="", *codes, prefix="", end="\n"):
+        """
+        Prints styled text to the console using ANSI escape codes.
+
+        Args:
+            - txt: The text to print. Defaults to an empty string.
+            - *codes: Variable-length list of ANSI codes (int/str)
+                        to style the text. Example: 31 for red, 1 for bold.
+            - prefix: String appended before the styled txt.
+            - end: String appended after the styled text.
+        Notes:
+            - ANSI codes are applied to the text only; 
+                `end` and 'prefix' are printed as-is.
+            - If no valid ANSI codes are provided, the text is printed as it is.
+            - Invalid ANSI codes (non-integer/non-string/non-ANSI) are ignored.
+        """
+        msg = Style.styled(txt, *codes)
+        sys.stdout.write(f"{prefix}{msg}{end}")
